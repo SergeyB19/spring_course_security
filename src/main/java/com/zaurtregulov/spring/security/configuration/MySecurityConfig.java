65c91138a0527b144bbb51e96c06461760e7dca0
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-06-29 13:54:44 +0300

    security

---
+++ src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityConfig.java
@@ -0,0 +1,8 @@
+package com.zaurtregulov.spring.security.configuration;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
